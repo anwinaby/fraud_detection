31e61e6ab64e39f3e54d548e43216ab917f41005
ANWIN ABY GEORGE <anwinaby2000@gmail.com>
2023-10-08 11:04:45 +0530

    corrected the code for sum function and multiplication

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print('adding all the data processing code related to fraud detection')
+a = 6
+b = 7 
+def sum_of (a,b):
+    return a+b 
+print(f'return the sum of {sum_of(a,b)}')
+
+def multiplication(a,b):
+    return b
+print(f'return the multiplication of {sum_of(a,b)}')
